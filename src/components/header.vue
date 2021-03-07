c099b392111a4f02b23d77aa30f07dfc0a6211a3
TereraiQubed <tapiwa.tererai@qubedlab.com>
2021-03-07 22:17:04 +0200

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header-area" id="headerArea">
+    <div class="container">
+      <!-- Paste your Header Content from here-->
+      <!-- # Header Five Layout-->
+      <!-- # Copy the code from here ...-->
+      <!-- Header Content-->
+      <div
+        class="header-content header-style-five position-relative d-flex align-items-center justify-content-between"
+      >
+        <!-- Logo Wrapper-->
+        <div class="logo-wrapper">
+          <a href="/"><img src="template/img/core-img/logo.png" alt="" /></a>
+        </div>
+        <!-- Navbar Toggler-->
+        <div class="navbar--toggler" id="affanNavbarToggler">
+          <span class="d-block"></span><span class="d-block"></span
+          ><span class="d-block"></span>
+        </div>
+      </div>
+      <!-- # Header Five Layout End-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Bottom_nav />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Bottom_nav from "../components/bottom_nav.vue";
+export default {
+  name: "Home",
+  components: {
+    Bottom_nav,
+  },
+};
+</script>
